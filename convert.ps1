1151f3f8e156f26977a255ecae1d02787ad79467
Harvy <harvinder@harvinderatwal.com>
2025-03-24 21:36:23 +0000

    Added streaming to process large files Added progress bar

--- convert.ps1
+++ convert.ps1
@@ -18,168 +18,118 @@ if (Test-Path -Path $outputFolder) {
 }
 
 
+
 foreach ($file in $csvFiles) {
     try {
-        # Import the CSV file
-        $csvContent = Import-Csv -LiteralPath $file.FullName
-        # Filter out rows where CellID is blank
-        $csvContent = $csvContent | Where-Object { -not [string]::IsNullOrWhiteSpace($_.CellID) }
+        # Import and process the CSV file as a stream
+        $reader = [System.IO.StreamReader]::new($file.FullName)
+        $writer = [System.IO.StreamWriter]::new((Join-Path -Path "$PSScriptRoot/output/" -ChildPath ([System.IO.Path]::GetFileNameWithoutExtension($file.Name) + "-FCX" + [System.IO.Path]::GetExtension($file.Name))))
 
-        # Output the number of lines processed
-        $lineCount = $csvContent.Count
-        Write-Output "Number of lines processed in $($file.Name): $lineCount"
+        # Read header line and write transformed header
+        $header = $reader.ReadLine()
+        $writer.WriteLine("Date,Time,Latitude,Longitude,Accuracy,Point,Source,Network,PLMN,Technology,Serving CID,LAC / TAC,Band Freq,Band Num,Channel,eNB / gNB,Sector ID,PSC / PCI,Power,Quality,N1_CID,N1_Channel,N1_PSC/PCI,N2_CID,N2_Channel,N2_PSC/PCI,N3_CID,N3_Channel,N3_PSC/PCI,N4_CID,N4_Channel,N4_PSC/PCI,N5_CID,N5_Channel,N5_PSC/PCI,N6_CID,N6_Channel,N6_PSC/PCI")
 
-        # Create a new array to store transformed data
-        $transformedData = $csvContent | ForEach-Object {
-            # Create new object with transformed data
-            [PSCustomObject]@{
-                'Date' = $_.Date
-                'Time' = $_.Time
-                'Latitude' = $_.Latitude 
-                'Longitude' = $_.Longitude
-                'Accuracy' = $_.Satellites
-                'Point' = ''
-                'Source' = $_.BoxID
-                'Network' = $(
-                    switch ($_.PLMN) {
-                        '23410' { 'O2 - UK' }
-                        '23415' { 'Vodafone UK' }
-                        '23420' { '3' }
-                        '23430' { 'EE' }
-                        '23433' { 'EE' }
-                        default { $_.Network }
-                    }
-                )
-                'PLMN' = $_.PLMN
-                'Technology' = $(
-                    if ($file.Name -like '*2G*') { '2G' }
-                    elseif ($file.Name -like '*3G*') { '3G' }
-                    elseif ($file.Name -like '*4G*') { '4G' }
-                    else { '' }
-                )
-                'Serving CID' = $_.CellID
-                'LAC / TAC' = $(
-                    if ($_.TAC) { $_.TAC }
-                    else { $_.LAC }
-                )
-                'Band Freq' = ''
-                'Band Num' = ''
-                'Channel' = $(
-                    if ($_.EARFCN) { $_.EARFCN } 
-                    else { $_.Frequency }
-                )
-                'eNB / gNB' = $_.eNB
-                'Sector ID' = $_.ShortCellID
-                'PSC / PCI' = $(
-                    if ($_.PSC) { $_.PSC }
-                    else { $_.PCI }
-                )
-                'Power' = $(
-                    if ($_.RSRP) { $_.RSRP }
-                    elseif ($_.RSCP) { $_.RSCP }
-                    else { '' }
-                )
-                'Quality' = $(
-                    if ($_.RSRQ) { $_.RSRQ }
-                    elseif ($_.ECIO) { $_.ECIO }
-                    else { '' }
-                )
-                'N1_CID' = $(
-                    if ($_.A1_CellID) { $_.A1_CellID }
-                    else { $_.N1_CellID }
-                )
-                'N1_Channel' = $(
-                    if ($_.N1_EARFCN) { $_.N1_EARFCN }
-                    elseif ($_.A1_Frequency) { $_.A1_Frequency }
-                    else { '' }
-                )
-                'N1_PSC/PCI' = $(
-                    if ($_.A1_PSC) { $_.A1_PSC }
-                    else { $_.N1_PCI }
-                )
-                'N2_CID' = $(
-                    if ($_.A2_CellID) { $_.A2_CellID }
-                    else { $_.N2_CellID }
-                )
-                'N2_Channel' = $(
-                    if ($_.N2_EARFCN) { $_.N2_EARFCN }
-                    elseif ($_.A2_Frequency) { $_.A2_Frequency }
-                    else { '' }
-                )
-                'N2_PSC/PCI' = $(
-                    if ($_.A2_PSC) { $_.A2_PSC }
-                    else { $_.N2_PCI }
-                )
-                'N3_CID' = $(
-                    if ($_.A3_CellID) { $_.A3_CellID }
-                    else { $_.N3_CellID }
-                )
-                'N3_Channel' = $(
-                    if ($_.N3_EARFCN) { $_.N3_EARFCN }
-                    elseif ($_.A3_Frequency) { $_.A3_Frequency }
-                    else { '' }
-                )
-                'N3_PSC/PCI' = $(
-                    if ($_.A3_PSC) { $_.A3_PSC }
-                    else { $_.N3_PCI }
-                )
-                'N4_CID' = $(
-                    if ($_.A4_CellID) { $_.A4_CellID }
-                    else { $_.N4_CellID }
-                )
-                'N4_Channel' = $(
-                    if ($_.N4_EARFCN) { $_.N4_EARFCN }
-                    elseif ($_.A4_Frequency) { $_.A4_Frequency }
-                    else { '' }
-                )
-                'N4_PSC/PCI' = $(
-                    if ($_.A4_PSC) { $_.A4_PSC }
-                    else { $_.N4_PCI }
-                )
-                'N5_CID' = $(
-                    if ($_.A5_CellID) { $_.A5_CellID }
-                    else { $_.N5_CellID }
-                )
-                'N5_Channel' = $(
-                    if ($_.N5_EARFCN) { $_.N5_EARFCN }
-                    elseif ($_.A5_Frequency) { $_.A5_Frequency }
-                    else { '' }
-                )
-                'N5_PSC/PCI' = $(
-                    if ($_.A5_PSC) { $_.A5_PSC }
-                    else { $_.N5_PCI }
-                )
-                'N6_CID' = $(
-                    if ($_.A6_CellID) { $_.A6_CellID }
-                    else { $_.N6_CellID }
-                )
-                'N6_Channel' = $(
-                    if ($_.N6_EARFCN) { $_.N6_EARFCN }
-                    elseif ($_.A6_Frequency) { $_.A6_Frequency }
-                    else { '' }
-                )
-                'N6_PSC/PCI' = $(
-                    if ($_.A6_PSC) { $_.A6_PSC }
-                    else { $_.N6_PCI }
-                )
+        $lineCount = 0
+        # Process each line
+        while (($line = $reader.ReadLine()) -ne $null) {
+            # Parse CSV line into object
+            $fields = $line -split ','
+            $row = @{}
+            $header -split ',' | ForEach-Object { $i = 0 } { $row[$_] = $fields[$i++] }
+
+            # Skip if CellID is empty
+            if ([string]::IsNullOrWhiteSpace($row.CellID)) { continue }
+
+            $lineCount++
+            # Get total line count if not already calculated
+            if (-not $totalLines) {
+                $totalLines = (Get-Content $file.FullName).Count - 1 # Subtract 1 for header
             }
-        }
-        # Append FCX to the output filename
-        $outputFileName = [System.IO.Path]::GetFileNameWithoutExtension($file.Name) + "-FCX" + [System.IO.Path]::GetExtension($file.Name)
-        $outputFilePath = Join-Path -Path "$PSScriptRoot/output/" -ChildPath $outputFileName
+            
+            # Calculate percentage
+            $percentComplete = [math]::Min(($lineCount / $totalLines) * 100, 100)
+            
+            # Show progress with percentage
+            Write-Progress -Activity "Processing $($file.Name)" -Status "Processing line $lineCount of $totalLines ($([math]::Round($percentComplete))%)" -PercentComplete $percentComplete
 
-        try {
-            # Export the transformed CSV content to a new file in the output folder
-            $transformedData | Export-Csv -Path $outputFilePath -NoTypeInformation -Force
-            
-            # Remove quotes from the output file if needed (compatible with all PowerShell versions)
-            $content = Get-Content $outputFilePath
-            $content | ForEach-Object { $_ -replace '\"([^\"]*?)\"', '$1' } | Set-Content $outputFilePath
-            
-            Write-Output "Processed file saved to: $outputFilePath"
-        } catch {
-            Write-Error "Failed to process file $($file.FullName): $_"
+            # Transform the data
+            $network = switch ($row.PLMN) {
+                '23410' { 'O2 - UK' }
+                '23415' { 'Vodafone UK' }
+                '23420' { '3' }
+                '23430' { 'EE' }
+                '23433' { 'EE' }
+                default { $row.Network }
+            }
+
+            $technology = if ($file.Name -like '*2G*') { '2G' }
+                         elseif ($file.Name -like '*3G*') { '3G' }
+                         elseif ($file.Name -like '*4G*') { '4G' }
+                         else { '' }
+
+            $channel = if ($row.EARFCN) { $row.EARFCN }
+                      else { $row.Frequency }
+
+            $power = if ($row.RSRP) { $row.RSRP }
+                    elseif ($row.RSCP) { $row.RSCP }
+                    else { '' }
+
+            $quality = if ($row.RSRQ) { $row.RSRQ }
+                      elseif ($row.ECIO) { $row.ECIO }
+                      else { '' }
+
+            # Write transformed line
+            $transformedLine = @(
+                $row.Date,
+                $row.Time, 
+                $row.Latitude,
+                $row.Longitude,
+                $row.Satellites,
+                '',
+                $row.BoxID,
+                $network,
+                $row.PLMN,
+                $technology,
+                $row.CellID,
+                $(if ($row.TAC) { $row.TAC } else { $row.LAC }),
+                '',
+                '',
+                $channel,
+                $row.eNB,
+                $row.ShortCellID,
+                $(if ($row.PSC) { $row.PSC } else { $row.PCI }),
+                $power,
+                $quality,
+                $(if ($row.A1_CellID) { $row.A1_CellID } else { $row.N1_CellID }),
+                $(if ($row.N1_EARFCN) { $row.N1_EARFCN } elseif ($row.A1_Frequency) { $row.A1_Frequency } else { '' }),
+                $(if ($row.A1_PSC) { $row.A1_PSC } else { $row.N1_PCI }),
+                $(if ($row.A2_CellID) { $row.A2_CellID } else { $row.N2_CellID }),
+                $(if ($row.N2_EARFCN) { $row.N2_EARFCN } elseif ($row.A2_Frequency) { $row.A2_Frequency } else { '' }),
+                $(if ($row.A2_PSC) { $row.A2_PSC } else { $row.N2_PCI }),
+                $(if ($row.A3_CellID) { $row.A3_CellID } else { $row.N3_CellID }),
+                $(if ($row.N3_EARFCN) { $row.N3_EARFCN } elseif ($row.A3_Frequency) { $row.A3_Frequency } else { '' }),
+                $(if ($row.A3_PSC) { $row.A3_PSC } else { $row.N3_PCI }),
+                $(if ($row.A4_CellID) { $row.A4_CellID } else { $row.N4_CellID }),
+                $(if ($row.N4_EARFCN) { $row.N4_EARFCN } elseif ($row.A4_Frequency) { $row.A4_Frequency } else { '' }),
+                $(if ($row.A4_PSC) { $row.A4_PSC } else { $row.N4_PCI }),
+                $(if ($row.A5_CellID) { $row.A5_CellID } else { $row.N5_CellID }),
+                $(if ($row.N5_EARFCN) { $row.N5_EARFCN } elseif ($row.A5_Frequency) { $row.A5_Frequency } else { '' }),
+                $(if ($row.A5_PSC) { $row.A5_PSC } else { $row.N5_PCI }),
+                $(if ($row.A6_CellID) { $row.A6_CellID } else { $row.N6_CellID }),
+                $(if ($row.N6_EARFCN) { $row.N6_EARFCN } elseif ($row.A6_Frequency) { $row.A6_Frequency } else { '' }),
+                $(if ($row.A6_PSC) { $row.A6_PSC } else { $row.N6_PCI })
+            ) -join ','
+
+            $writer.WriteLine($transformedLine)
         }
+        Write-Progress -Activity "Processing $($file.Name)" -Completed
+
+        Write-Output "Number of lines processed in $($file.Name): $lineCount"
+        Write-Output "Processed file saved to: $($writer.BaseStream.Name)"
+
+        # Clean up
+        $reader.Close()
+        $writer.Close()
 
     } catch {
         Write-Error "Failed to import or process file $($file.FullName): $_"
